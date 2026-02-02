cf7f34e2d022b4789972d8e1c7301341894b9aae
colinknox <colinwknox@gmail.com>
2026-02-02 07:06:28 -0500

    completed Character class

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ class Character:
         self.name = name
         self.health = health
 
+    def get_stats(self):
+        return f"Name: {self.name}, Health: {self.health}"
+    
+    def take_damage(self, amount):
+        self.health -= amount
+
+        if self.health <= 0:
+            self.health = 0
+
 
 
 yusuke = Character("Yusuke", 10_000)
 
-print(yusuke.name)
-print(yusuke.health)
+print(f"Before health: {yusuke.health}")
+yusuke.take_damage(9_999)
+print(f"After health: {yusuke.health}")
