e390faf9ea13170bfab029322329a74f5016f0d5
ColinKnox <colinwknox@gmail.com>
2026-02-02 10:44:37 -0500

    completed Warrior class

--- main.py
+++ main.py
@@ -12,10 +12,32 @@ class Character:
         if self.health <= 0:
             self.health = 0
 
+class Warrior(Character):
+    def __init__(self, name, health, strength):
+        super().__init__(name, health)
+        self.strength = strength
+        self.__armor = 10
+
+    def attack(self):
+        return self.strength * 2
+
+    def get_armor(self):
+        return self.__armor
+    
+    def repair_armor(self, amount):
+        self.__armor += amount
 
 
-yusuke = Character("Yusuke", 10_000)
+# yusuke = Character("Yusuke", 10_000)
+# print(f"Before health: {yusuke.health}")
+# yusuke.take_damage(9_999)
+# print(f"After health: {yusuke.health}")
 
-print(f"Before health: {yusuke.health}")
-yusuke.take_damage(9_999)
-print(f"After health: {yusuke.health}")
+conan = Warrior("Conan", 100, 25)
+# print(conan.name)
+# print(conan.health)
+# print(conan.strength)
+# print(conan.attack())
+print(conan.get_armor())
+print(conan.repair_armor(5))
+print(conan.get_armor())
